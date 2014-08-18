68fdabc5d4b85e363144a6ff19b2c347c39684ea
fedorino-alex <fedorino.alex@gmail.com>
2014-08-19 01:15:32 +0400

    Add web project

---
+++ Eyelock/App.js
@@ -0,0 +1,194 @@
+﻿var App = function (settings) 
+{
+    this._settings = 
+    {
+        AutoUpdate: true,
+        AutoUpdateInterval: 5000, //ms
+        CurrentItem: null
+    };
+};
+
+var appP = App.prototype;
+
+appP.init = function()
+{
+    this._initPopupSettings();
+    this._initLayout();
+    this._bind();
+
+    this.goToNextListItem();
+};
+
+appP._initPopupSettings = function()
+{
+     webix.ui(
+        {
+            id: 'settings_popup',
+            view: 'popup',
+            head:false, 
+            width: 200,
+            body:
+            {
+                rows: 
+                [
+                    {
+                        view: 'checkbox', 
+                        id: 'autoUpdateCheckbox',
+                        label: 'Enable auto update',
+                        value: true,
+                        labelWidth: 160
+                    }
+                ]
+			}
+        }).hide();
+};
+
+appP._initLayout = function()
+{
+	webix.ui({
+		rows: [
+			{ 
+				cols: 
+				[
+					{
+						height: 80, 
+						view: "label",  
+						label: "<span class='TitleLabel'>Eyelock query management application</span>",
+						type: 'header'
+					},
+					{
+						view: 'button',
+                        width: 80,
+                        type: 'image',
+                        template:function(obj, common)
+                        {
+                            obj.cheight = obj.dheight = 48;
+			                return "<div class='webix_el_box' style='width:52px; height:54px; margin: 13px 14px;'>"+common._inputTemplate(obj, common)+"</div>";
+		                },
+                        image: 'img/settings.png',
+                        popup: 'settings_popup'
+					}
+				]
+			},
+			{ 
+				cols: [
+					{ 
+						view: "list",
+						id: "queue",
+						minWidth: 320,
+						width: 320,
+						data: example_data,
+						template: "<div><b>#title#</b><div>#data.firstName# #data.lastName# #data.dob#</div></div>",
+						select: true,
+						type: 
+						{
+							height: 'auto',
+							classname: function (obj)
+							{
+								var css = webix.ui.list.prototype.type.classname.apply(this, arguments);
+								if (obj && obj.processed)
+									css = css + ' processed';
+								return css;
+							}
+						},
+					},
+					{ view : "resizer" },
+					{
+						rows: 
+						[{
+							id: "tab_view",
+							view: "multiview",
+							cells: 
+							[
+								{ id: 'null', template: 'EMPTY' },
+								{ id: 'add', template: 'add form' },
+								{ id: 'view', template: 'preview form' }
+							]
+						},
+						{
+							cols: 
+							[
+								{
+									view: 'button',
+                                    id: 'processButton',
+									value: 'Обработать',
+									inputWidth: 150,
+									enabled: false,
+                                    align: 'right'
+								}
+							]
+					}]
+					}
+				]
+			}
+		]
+    });
+};
+
+
+appP._getAddForm = function()
+{
+    return 
+    {
+        view: 'form'
+ //       elements: 
+    };
+};
+
+appP._getPreviewForm = function()
+{
+
+};
+
+appP._bind = function()
+{
+    // набор связываний и обработчиков событий
+
+    $$("tab_view").bind($$("queue"), "event", function(data) { return data || { event: 'empty'}; });
+    $$('autoUpdateCheckbox').attachEvent('onChange', this._onAutoUpdateChanged.bind(this));
+    $$('processButton').attachEvent('onItemClick', this._onProcessClick.bind(this));
+
+};
+
+appP.goToNextListItem = function()
+{
+    var list = $$("queue"),
+        current = list.getCursor() || list.data.getFirstId();
+
+    while (current && list.data.getItem(current).processed)
+        current = list.data.getNextId(current);
+
+    if (current)
+        list.select(current);
+    else 
+        list.unselectAll();
+};
+
+appP._doProcess = function()
+{
+    var list = $$("queue"),
+        current = list.getCursor();
+
+    if (current)
+        list.data.getItem(current).processed = true;
+
+    // слать запрос.
+
+    list.refresh();
+};
+
+// ------------------- Event handlers -------------------------
+
+appP._onAutoUpdateChanged = function(value)
+{
+    this._settings.AutoUpdate = !!value;
+};
+
+appP._onProcessClick = function()
+{
+    this._doProcess();
+    this.goToNextListItem();
+};
+
+appP = null;
+
---
+++ Eyelock/data.js
@@ -0,0 +1,7 @@
+var example_data = 
+[
+	{ event: 'view', title:'Просмотр профиля', processed: true, data: { firstName: 'Иван', lastName: 'Петров', dob: '02/20/1988'} },
+	{ event: 'add', title:'Добавление профиля', processed: true, data: { firstName: 'Добавленный', lastName: 'Чувак', dob: '01/10/1998'} },
+	{ event: 'view', title:'Просмотр профиля', processed: false, data: { firstName: 'Иван', lastName: 'Петров', dob: '02/20/1988'} },
+	{ event: 'add', title:'Добавление профиля', processed: false, data: { firstName: '', lastName: '', dob: ''} }
+];
